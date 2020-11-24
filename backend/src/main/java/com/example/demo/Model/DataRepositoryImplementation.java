f3ca765831e44ef943fe243e38b4b4f1145d160f
StefanMaksovic <smaksovic1118rn@raf.rs>
2020-11-24 23:18:31 +0100

    Gotov query4

--- backend/src/main/java/com/example/demo/Model/DataRepositoryImplementation.java
+++ backend/src/main/java/com/example/demo/Model/DataRepositoryImplementation.java
@@ -64,23 +64,26 @@ public class DataRepositoryImplementation implements DataRepository{
     }
 
     public String getQuery3 (Player player) throws JSONException {
-        long sumPoints = player.getSumPoints();
+        int[] sumPoints = player.getSumPoints();
         double avgPoints;
-        long sumAssists = player.getSumAssists();
         double avgAssists;
-        long sumJumps = player.getSumJumps();
         double avgJumps;
-        avgPoints = (double)sumPoints/player.getGamesPlayed().size();
-        avgAssists = (double)sumAssists/player.getGamesPlayed().size();
-        avgJumps = (double)sumJumps/player.getGamesPlayed().size();
+
+        /*long sumAssists = player.getSumAssists();
+        long sumJumps = player.getSumJumps();*/
+
+        avgPoints = (double)sumPoints[0]/player.getGamesPlayed().size();
+        avgAssists = (double)sumPoints[1]/player.getGamesPlayed().size();
+        avgJumps = (double)sumPoints[2]/player.getGamesPlayed().size();
+
         JSONObject jo = new JSONObject();
         jo.put("first name", player.getFirstName());
         jo.put("last name", player.getLastName());
         jo.put("sum points", sumPoints);
         jo.put("avg points", avgPoints);
-        jo.put("sum assists", sumAssists);
+        jo.put("sum assists", sumPoints[1]);
         jo.put("avg assists", avgAssists);
-        jo.put("sum jumps", sumJumps);
+        jo.put("sum jumps", sumPoints[2]);
         jo.put("avg jumps", avgJumps);
         return jo.toString();
     }
@@ -94,16 +97,54 @@ public class DataRepositoryImplementation implements DataRepository{
         return retList;
     }
 
+    public List<Player> getMaximumScore (List<Player> players) {
+        List<Player> maxList = new ArrayList<>();
+        maxList.add(players.get(0));
+        int [] maxArray = players.get(0).getSumPoints();
+        for (int i=1; i < players.size(); i++) {
+            int[] currArray = players.get(i).getSumPoints();
+            if (maxArray[0] < currArray[0] ||
+                    (maxArray[0] == currArray[0] && maxArray[1] < currArray[1]) ||
+                    (maxArray[0] == currArray[0] && maxArray[1] == currArray[1] && maxArray[2] < currArray[2])) {
+                maxArray = currArray;
+                maxList.clear();
+                maxList.add(players.get(i));
+            }
+            else if (maxArray[0] < currArray[0] && maxArray[1] < currArray[1] && maxArray[2] < currArray[2]){
+                maxList.add(players.get(i));
+            }
+        }
+        return maxList;
+    }
 
 
-    public void getQuery4 () {
+    public List<JSONObject> getQuery4 () throws JSONException {
         List<Player> pointGuard = getByPlayerPosition(Position.POINT_GUARD);
         List<Player> shootingGuard = getByPlayerPosition(Position.SHOOTING_GUARD);
         List<Player> smallGuard = getByPlayerPosition(Position.SMALL_FORWARD);
         List<Player> powerGuard = getByPlayerPosition(Position.POWER_FORWARD);
         List<Player> center = getByPlayerPosition(Position.CENTER);
 
+        pointGuard = getMaximumScore(pointGuard);
+        shootingGuard = getMaximumScore(shootingGuard);
+        smallGuard = getMaximumScore(smallGuard);
+        powerGuard = getMaximumScore(powerGuard);
+        center = getMaximumScore(center);
 
+        List<Player> retList = pointGuard;
+        retList.addAll(shootingGuard);
+        retList.addAll(smallGuard);
+        retList.addAll(powerGuard);
+        retList.addAll(center);
+
+        List<JSONObject> listJo = new ArrayList<>();
+        for (int i=0; i<retList.size(); i++) {
+            JSONObject jo = new JSONObject();
+            String name = retList.get(i).getFirstName() + retList.get(i).getLastName();
+            jo.put(name, retList.get(i).getPosition());
+            listJo.add(jo);
+        }
+        return listJo;
     }
 
     public void getQuery5 () {
