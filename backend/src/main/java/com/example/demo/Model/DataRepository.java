eb31f8bc973f6e22966f790041a673deb1262c53
StefanMaksovic <smaksovic1118rn@raf.rs>
2020-11-25 13:52:57 +0100

    Popunjen interfejs

--- backend/src/main/java/com/example/demo/Model/DataRepository.java
+++ backend/src/main/java/com/example/demo/Model/DataRepository.java
@@ -1,5 +1,12 @@
 package example.demo.Model;
 
-public interface DataRepository {
+import org.json.JSONException;
 
+public interface DataRepository {
+    public String getQuery1() throws JSONException;
+    public String getQuery2(Game game) throws JSONException;
+    public String getQuery3(Player player) throws JSONException;
+    public String getQuery4() throws JSONException;
+    public String getQuery5() throws JSONException;
+    public String getQuery6() throws JSONException;
 }
---
+++ backend/src/main/java/com/example/demo/Model/Test.java
@@ -0,0 +1,5 @@
+package example.demo.Model;
+
+public class Test {
+
+}
